603441a598b948abd0361cdae3ac1a630b5d9e01
imransihab0 <imran.sihab.0@gmail.com>
2024-03-09 01:22:59 +0600

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-import 'package:chattysimple/screens/home.dart';
+import 'dart:developer';
 import 'package:chattysimple/screens/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    log('Firebase initialization error: $e');
+  }
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: splashScreen(),
